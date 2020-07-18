4b381f4dbe7d04c1e96af0d5fb95cb0731bdc8da
Ivan Ivanov <buzzlighter97@gmail.com>
2020-07-18 20:38:31 +0300

    First commit for the 4th sprint

---
+++ posts/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from .models import Post, Group
+from django.shortcuts import redirect
+
+import datetime as dt
+
+def index(request):
+    latest = Post.objects.all()[:11]
+    return render(request, "index.html", {"posts": latest})
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+
+    posts = group.groups_posts.all()[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
+
+@login_required
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('index')
+        return render(request, 'new_post.html', {'form': form})
+    form = PostForm()
+    return render(request, 'new_post.html', {'form': form})
